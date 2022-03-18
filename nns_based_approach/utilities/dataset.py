a44f58c7ed39b5c5e236cfa9075ebfa1eed4f500
David Schote <david.schote@ptb.de>
2022-03-18 10:32:45 +0000

    Return acquisition model in dataset

--- nns_based_approach/utilities/dataset.py
+++ nns_based_approach/utilities/dataset.py
@@ -1,13 +1,13 @@
 import os
 import nibabel as nib
-import sirf.Gadgetron as pMR
+import sirf.Gadgetron as mr
 from glob import glob
 from torch.utils.data import Dataset
 
 
 class ImageDataset(Dataset):
     """ Dataset class for torch data loader """
-    def __init__(self, input_data_path, label_data_path, transform=None):
+    def __init__(self, input_data_path, label_data_path):
         
         self.input_dir = input_data_path
         self.label_dir = label_data_path
@@ -19,8 +19,6 @@ class ImageDataset(Dataset):
             raise ValueError(f"Number of inputs and labels don't agree. {len(input_files)} inputs and {len(label_files)} labels.")
         
         self.data_len = len(input_files)
-        
-        self.transform = transform
 
     def __len__(self):
         return self.data_len
@@ -31,11 +29,13 @@ class ImageDataset(Dataset):
         label_data = nib.load(label_path)
         
         input_path = os.path.join(self.input_dir, f"y_{idx}.h5")
-        input_data = pMR.AcquisitionData(input_path)
+        input_data = mr.AcquisitionData(input_path)
         
-        # TODO: Create an acquisition model from acquisition data object
-        
-        # if self.transform:
-        #    img = self.transform(img)
+        csm = mr.CoilSensitivityData()
+        csm.smoothness = 200 # ?
+        csm.calculate(input_data)
+
+        model = mr.AcquisitionModel(input_data, csm)
+        model.set_coil_sensitivity_maps(csm)
             
-        return input_data, label_data
+        return input_data, label_data, model
