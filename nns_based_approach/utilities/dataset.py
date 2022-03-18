00867a7bd8be32e8985b7f0be11ef3f0c052658d
David Schote <david.schote@ptb.de>
2022-03-18 10:14:17 +0000

    Training script and data loader

--- nns_based_approach/utilities/dataset.py
+++ nns_based_approach/utilities/dataset.py
@@ -1,32 +1,41 @@
 import os
-# import nibabel as nib
+import nibabel as nib
+import sirf.Gadgetron as pMR
 from glob import glob
 from torch.utils.data import Dataset
-# import h5py
-import sirf.Gadgetron as pMR
 
 
 class ImageDataset(Dataset):
     """ Dataset class for torch data loader """
-    def __init__(self, data_dir, transform=None):
+    def __init__(self, input_data_path, label_data_path, transform=None):
         
-        self.data_dir = data_dir
-        self.files = glob(f"{self.data_dir}y_*.h5")
+        self.input_dir = input_data_path
+        self.label_dir = label_data_path
+        
+        input_files = glob(f"{self.input_dir}y_*.h5")
+        label_files = glob(f"{self.label_dir}img_*.nii")
+        
+        if not len(input_files) == len(label_files):
+            raise ValueError(f"Number of inputs and labels don't agree. {len(input_files)} inputs and {len(label_files)} labels.")
+        
+        self.data_len = len(input_files)
+        
         self.transform = transform
 
     def __len__(self):
-        return len(self.files)
+        return self.data_len
 
     def __getitem__(self, idx):
         
-        # img_path = os.path.join(self.data_dir, f"img_{idx}.nii")
-        # img = nib.load(img_path)
-        # img = h5py.File(img_path, "r")
+        label_path = os.path.join(self.label_dir, f"img_{idx}.nii")
+        label_data = nib.load(label_path)
         
-        data_path = os.path.join(self.data_dir, f"y_{idx}.h5")
-        acq_data = pMR.AcquisitionData(data_path)
+        input_path = os.path.join(self.input_dir, f"y_{idx}.h5")
+        input_data = pMR.AcquisitionData(input_path)
         
-        if self.transform:
-            img = self.transform(img)
+        # TODO: Create an acquisition model from acquisition data object
+        
+        # if self.transform:
+        #    img = self.transform(img)
             
-        return acq_data
+        return input_data, label_data
