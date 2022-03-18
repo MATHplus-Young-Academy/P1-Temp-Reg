6749ad85dfe3837d33bbfb7fe41ef2d105eb55c6
David Schote <david.schote@ptb.de>
2022-03-18 15:57:11 +0000

    Collate function for data loader

--- nns_based_approach/utilities/dataset.py
+++ nns_based_approach/utilities/dataset.py
@@ -4,11 +4,12 @@ import sirf.Gadgetron as mr
 import torch
 from glob import glob
 from torch.utils.data import Dataset
+import torchvision.transforms as transforms
 
 
 class ImageDataset(Dataset):
     """ Dataset class for torch data loader """
-    def __init__(self, input_data_path, label_data_path):
+    def __init__(self, input_data_path, label_data_path, transform=None):
         
         self.input_dir = input_data_path
         self.label_dir = label_data_path
@@ -20,6 +21,11 @@ class ImageDataset(Dataset):
             raise ValueError(f"Number of inputs and labels don't agree. {len(input_files)} inputs and {len(label_files)} labels.")
         
         self.data_len = len(input_files)
+        
+        if transform is not None:
+            self.transform = transform
+        else:     
+            self.transform = transforms.Compose([transforms.ToTensor()])
 
     def __len__(self):
         return self.data_len
@@ -41,3 +47,8 @@ class ImageDataset(Dataset):
         model.set_coil_sensitivity_maps(csm)
             
         return input_data, label_tensor, model
+
+
+def collate_func(batch):
+    input, label, model = batch[0]
+    return list([input, label, model])
